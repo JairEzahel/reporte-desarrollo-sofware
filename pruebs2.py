82fd375d81c705bead11ded1ef83b145d1c92ae1
JairEzahel <86019946+JairEzahel@users.noreply.github.com>
2021-06-16 14:13:28 -0500

    Add files via upload

---
+++ pruebs2.py
@@ -0,0 +1,19 @@
+{
+    "clients": [
+        {
+            "marca": "tp-link",
+            "tecnologias_c": "router",
+            "tecnologias_especiales": "Firewall NG"
+        },
+        {
+            "marca": "cisco",
+            "tecnologias_c": "servidores",
+            "tecnologias_especiales": "Soluciones SD-WAN"
+        },
+        {
+            "marca": "photon",
+            "tecnologias_c": "servidores",
+            "tecnologias_especiales": "Corteza XDR"
+        }
+    ]
+}
